d55f6ffdb91576c0dc995e0601b07211f0d22a08
jhedoner <jhon93.hg@gmail.com>
2022-07-06 04:57:56 -0500

    sixth commit

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -59,6 +59,13 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var array2=[];
+  for(i=0; i<array.length; i++){
+    if(array[i][0] === "a"){
+      array2.push(array[i]);
+    }
+  }
+  return array2;
 }
 
 // No modificar nada debajo de esta línea
